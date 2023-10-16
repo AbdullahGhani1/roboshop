dff6774b4a42a26f841feb3b8007a2a93f13279e
abdullahghani1 <engr.abdullahghani@gmail.com>
2023-10-17 01:21:58 +0500

    catalogue echo steps

--- roboshop-shell/catalogue.sh
+++ roboshop-shell/catalogue.sh
@@ -1,19 +1,39 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo ">>>>>>>>>> Create Catalogue Service   <<<<<<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
 
+echo ">>>>>>>>>> Create MongoDB Repo <<<<<<<<<<"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo ">>>>>>>>>> Install NodeJS Repos <<<<<<<<<<"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo ">>>>>>>>>> Install NodeJS <<<<<<<<<<"
 yum install nodejs -y
+
+echo ">>>>>>>>>> Create Application User <<<<<<<<<<"
 useradd roboshop
+
+echo ">>>>>>>>>> Create App Directory <<<<<<<<<<"
 mkdir /app
+
+echo ">>>>>>>>>> Download Application Content <<<<<<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>>>>>> Extract Application Content <<<<<<<<<<"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
+
+echo ">>>>>>>>>> Download NodeJS Dependencies <<<<<<<<<<"
 npm install
 
+echo ">>>>>>>>>> Install Mongo Client <<<<<<<<<<"
 yum install mongodb-org-shell -y
+
+echo ">>>>>>>>>> Load Catalogue Schema  <<<<<<<<<<"
 mongo --host mongodb.domain.com </app/schema/catalogue.js
 
+echo ">>>>>>>>>> Start Catalogue Service <<<<<<<<<<"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
